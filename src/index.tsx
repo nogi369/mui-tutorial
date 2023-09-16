1c81ee73a81228594de1afaa2896e46f2541cf89
nogi <Createc369.Re@gmail.com>
2023-09-16 17:49:31 +0900

    add: sign-in

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
+// https://amateur-engineer.com/react-material-ui/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './SignIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>
 );
 
